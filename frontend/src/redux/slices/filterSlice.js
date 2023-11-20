1fb3ed0e69da995bbb3e097c0ba88227010c7d07
Sergey87 <serge.techno@gmail.com>
2023-11-20 23:14:29 +0300

    added createSlice in project

---
+++ frontend/src/redux/slices/filterSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  title: '',
+};
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: initialState,
+  reducers: {
+    setTitleFilter: (state, action) => {
+      return { ...state, title: action.payload };
+    },
+  },
+});
+
+// console.log(filterSlice.actions);
+// console.log(filterSlice.actions.setTitleFilter());
+
+export const setTitleFilter = filterSlice.actions.setTitleFilter;
+// or
+// const { setTitleFilter } = filterSlice.actions;
+
+export default filterSlice.reducer;
