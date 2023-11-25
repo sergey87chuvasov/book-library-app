ab7cebbec8a6bbc84193c9e9e8a2224cefa72566
Sergey87 <serge.techno@gmail.com>
2023-11-25 14:33:57 +0300

    added highlighting of parts of words when filtering

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -5,6 +5,7 @@ import { BsBookmarkStarFill, BsBookmarkStar } from 'react-icons/bs';
 import {
   selectTitleFilter,
   selectAuthorFilter,
+  selectOnlyFavoriteFilter,
 } from '../../redux/slices/filterSlice';
 import './BookList.css';
 
@@ -12,6 +13,7 @@ const BookList = () => {
   const books = useSelector((state) => state.books); // название доджно совпадать с reducer, we choose only part of our state
   const titleFilter = useSelector(selectTitleFilter);
   const authorFilter = useSelector(selectAuthorFilter);
+  const onlyFavoriteFilter = useSelector(selectOnlyFavoriteFilter);
   const dispatch = useDispatch();
 
   const handleDeleteBook = (id) => {
@@ -35,8 +37,28 @@ const BookList = () => {
     const matchesAuthor = book.author
       .toLowerCase()
       .includes(authorFilter.toLowerCase());
-    return matchesTitle && matchesAuthor;
+
+    const matchesFavorite = onlyFavoriteFilter ? book.isFavorite : true;
+
+    return matchesTitle && matchesAuthor && matchesFavorite;
   });
+
+  const highlightMatch = (text, filter) => {
+    if (!filter) return text;
+
+    const regex = new RegExp(`(${filter})`, 'gi');
+    // console.log(text.split(regex));
+    return text.split(regex).map((substring, i) => {
+      if (substring.toLowerCase() === filter.toLowerCase()) {
+        return (
+          <span key={i} className='highlight'>
+            {substring}
+          </span>
+        );
+      }
+      return substring;
+    });
+  };
 
   return (
     <div className='app-block book-list'>
@@ -49,7 +71,8 @@ const BookList = () => {
           {filteredBooks.map((book, i) => (
             <li key={book.id}>
               <div className='book-info'>
-                {++i}. {book.title} by <strong>{book.author}</strong>
+                {++i}. {highlightMatch(book.title, titleFilter)} by{' '}
+                <strong>{highlightMatch(book.author, authorFilter)}</strong>
               </div>
               <div className='book-actions'>
                 <span onClick={() => handleToggleFavorite(book.id)}>
