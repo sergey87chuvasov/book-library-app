b7c065ccb320d5df36a8b585a0f115162ecf44cd
Sergey87 <serge.techno@gmail.com>
2023-11-26 20:59:17 +0300

    added refactoring reducer for books with createSlice - 1 part

---
+++ frontend/src/redux/slices/booksSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = [];
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
+  reducers: {
+    addBook: (state, action) => {
+      state.push(action.payload);
+    },
+    deleteBook: (state, action) => {
+      return state.filter((book) => book.id !== action.payload);
+    },
+    toggleFavorite: (state, action) => {
+      state.forEach((book) => {
+        if (book.id === action.payload) {
+          book.isFavorite = !book.isFavorite;
+        }
+      });
+      // return state.map((book) =>
+      //   book.id === action.payload
+      //     ? { ...book, isFavorite: !book.isFavorite }
+      //     : book
+      // );
+    },
+  },
+});
+
+export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
+
+export default booksSlice.reducer;
