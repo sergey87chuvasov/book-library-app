bc77a385099bdd5a081d9f260c7558069ecf8c9f
Sergey87 <serge.techno@gmail.com>
2023-12-04 01:18:34 +0300

    added to show error if adding a book is unsuccessful

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,14 +1,22 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import createBookWithID from '../../utils/createBookWithID';
+import { setError } from './errorSlice';
 
 const initialState = [];
 
-export const fetchBook = createAsyncThunk('books/fetchBook', async () => {
-  const res = await axios.get('http://localhost:4000/random-book');
-  // console.log(res.data);
-  return res.data;
-});
+export const fetchBook = createAsyncThunk(
+  'books/fetchBook',
+  async (url, thunkAPI) => {
+    try {
+      const res = await axios.get(url);
+      return res.data;
+    } catch (error) {
+      thunkAPI.dispatch(setError(error.message));
+      throw error;
+    }
+  }
+);
 
 const booksSlice = createSlice({
   name: 'books',
@@ -30,7 +38,6 @@ const booksSlice = createSlice({
   },
   extraReducers: (builder) => {
     builder.addCase(fetchBook.fulfilled, (state, action) => {
-      // state.push(action.payload);
       if (action.payload.title && action.payload.author) {
         state.push(createBookWithID(action.payload, 'API'));
       }
