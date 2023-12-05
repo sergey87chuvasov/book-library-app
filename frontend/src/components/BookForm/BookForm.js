458e8a4a90807f322c2b2baedefe582d6e323ba8
Sergey87 <serge.techno@gmail.com>
2023-12-05 20:44:52 +0300

    fixed project

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -33,7 +33,7 @@ const BookForm = () => {
   };
 
   const handleAddRandomBookViaAPI = () => {
-    dispatch(fetchBook('http://localhost:4000/random-book'));
+    dispatch(fetchBook('http://localhost:4000/random-book-delayed'));
   };
 
   return (
